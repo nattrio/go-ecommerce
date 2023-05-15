6c21c4259b8af444f646cf873f709dc073411006
nattrio <jirapat.nattrio@gmail.com>
2023-05-15 20:00:44 +0700

    feat: add cors

--- modules/servers/module.go
+++ modules/servers/module.go
@@ -2,6 +2,9 @@ package servers
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/nattrio/go-ecommerce/modules/middleware/middlewareHandlers"
+	"github.com/nattrio/go-ecommerce/modules/middleware/middlewareRepositories"
+	"github.com/nattrio/go-ecommerce/modules/middleware/middlewareUsecases"
 	"github.com/nattrio/go-ecommerce/modules/monitor/monitorHandlers"
 )
 
@@ -10,15 +13,24 @@ type IModuleFactory interface {
 }
 
 type moduleFactory struct {
-	r fiber.Router // r is a fiber.Router
-	s *server      // s is a pointer to a server
+	r   fiber.Router                          // r is a fiber.Router
+	s   *server                               // s is a pointer to a server
+	mid middlewareHandlers.IMiddlewareHandler // mid is a middlewareHandlers
 }
 
-func InitModule(r fiber.Router, s *server) IModuleFactory {
+func InitModule(r fiber.Router, s *server, mid middlewareHandlers.IMiddlewareHandler) IModuleFactory {
 	return &moduleFactory{
-		r: r,
-		s: s,
+		r:   r,
+		s:   s,
+		mid: mid,
 	}
+}
+
+func InitMiddlewares(s *server) middlewareHandlers.IMiddlewareHandler {
+	repository := middlewareRepositories.MiddlewareRepository(s.db)
+	usecase := middlewareUsecases.MiddlewareUsecase(repository)
+	handler := middlewareHandlers.MiddlewareHandler(s.cfg, usecase)
+	return handler
 }
 
 func (m *moduleFactory) MonitorModule() {
