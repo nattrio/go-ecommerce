385eee4b588adc5eefbab177145bbfcdf358626e
nattrio <jirapat.nattrio@gmail.com>
2023-05-15 13:10:00 +0700

    feat: health check monitoring

---
+++ modules/monitor/MonitorHandlers/monitor_handler.go
@@ -0,0 +1,30 @@
+package monitorHandlers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/nattrio/go-ecommerce/config"
+	"github.com/nattrio/go-ecommerce/modules/monitor"
+)
+
+type IMonitorHandler interface {
+	HeathCheck(c *fiber.Ctx) error
+}
+
+type monitorHandler struct {
+	cfg config.IConfig
+}
+
+func MonitorHandler(cfg config.IConfig) IMonitorHandler {
+	return &monitorHandler{
+		cfg: cfg,
+	}
+}
+
+func (h *monitorHandler) HeathCheck(c *fiber.Ctx) error {
+	res := &monitor.Monitor{
+		Name:    h.cfg.App().Name(),
+		Version: h.cfg.App().Version(),
+	}
+
+	return c.Status(fiber.StatusOK).JSON(res)
+}
