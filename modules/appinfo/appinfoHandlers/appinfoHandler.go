89435d7eaec12e01d4500dc4cfb0bc468a056de3
nattrio <jirapat.nattrio@gmail.com>
2023-05-28 18:52:22 +0700

    feat: insert category

--- modules/appinfo/appinfoHandlers/appinfoHandler.go
+++ modules/appinfo/appinfoHandlers/appinfoHandler.go
@@ -21,6 +21,7 @@ const (
 type IAppinfoHandler interface {
 	GenerateApiKey(c *fiber.Ctx) error
 	FindCategory(c *fiber.Ctx) error
+	AddCategory(c *fiber.Ctx) error
 }
 
 type appinfoHandler struct {
@@ -77,4 +78,31 @@ func (h *appinfoHandler) FindCategory(c *fiber.Ctx) error {
 		).Res()
 	}
 	return entities.NewResponse(c).Success(fiber.StatusOK, category).Res()
+}
+
+func (h *appinfoHandler) AddCategory(c *fiber.Ctx) error {
+	req := make([]*appinfo.Category, 0)
+	if err := c.BodyParser(&req); err != nil {
+		return entities.NewResponse(c).Error(
+			fiber.ErrBadRequest.Code,
+			string(addCategoryErr),
+			err.Error(),
+		).Res()
+	}
+	if len(req) == 0 {
+		return entities.NewResponse(c).Error(
+			fiber.ErrBadRequest.Code,
+			string(addCategoryErr),
+			"categories request are empty",
+		).Res()
+	}
+
+	if err := h.appinfoUsecase.InsertCategory(req); err != nil {
+		return entities.NewResponse(c).Error(
+			fiber.ErrInternalServerError.Code,
+			string(addCategoryErr),
+			err.Error(),
+		).Res()
+	}
+	return entities.NewResponse(c).Success(fiber.StatusCreated, req).Res()
 }
--- modules/servers/module.go
+++ modules/servers/module.go
@@ -73,6 +73,8 @@ func (m *moduleFactory) AppInfoModule() {
 
 	router := m.r.Group("/appinfo")
 
+	router.Post("/categories", m.mid.JwtAuth(), m.mid.Authorize(2), handler.AddCategory)
+
 	router.Get("/categories", m.mid.ApiKeyAuth(), handler.FindCategory)
 	router.Get("/apikey", m.mid.JwtAuth(), m.mid.Authorize(2), handler.GenerateApiKey)
 }
